0497d1f6a45ee8368fcd0b80c568e37c7f455154
Humbert0 <matheushumberto520@gmail.com>
2024-06-23 20:11:05 -0400

    feat(checkout/button): will add a button go to back /home

--- app/routes/checkout/route.tsx
+++ app/routes/checkout/route.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
-import { ArrowsClockwise, CreditCard, CurrencyDollar, ListMagnifyingGlass, MapPinLine, Money, PixLogo } from "@phosphor-icons/react";
+import { ArrowLeft, ArrowsClockwise, CreditCard, CurrencyDollar, ListMagnifyingGlass, MapPinLine, Money, PixLogo } from "@phosphor-icons/react";
 import { json, redirect, type ActionFunctionArgs, type MetaFunction } from "@remix-run/node";
-import { Form, useActionData, useFetcher, useNavigation } from "@remix-run/react";
+import { Form, Link, useActionData, useFetcher, useNavigation } from "@remix-run/react";
 import { useState } from "react";
 import { z } from "zod";
 import { Product } from "../../components/product";
@@ -71,84 +71,88 @@ export default function Checkout() {
   };
   
   return (
-    <div className="md:grid md:grid-cols-[55%_45%] gap-5">
-      <div className="flex flex-col gap-4 py-12">
-        <h2 className="font-black text-xl font-title">Complete seu pedido</h2>
+    <>
+      <Link to="/home">
+        <ArrowLeft size={32} />
+      </Link>
+      <div className="md:grid md:grid-cols-[55%_45%] gap-5">
+        <div className="flex flex-col gap-4 py-10">
+          <h2 className="font-black text-xl font-title">Complete seu pedido</h2>
 
-        <div className="bg-stone-100 px-4 pt-9 pb-6 rounded-lg">
-          <div className="flex justify-between items-start">
-            <div className="flex gap-2 pb-5">
-              <MapPinLine className="text-amber-600" size={20} />
-              <div>
-                <h3 className="text-base">Endereço de entrega</h3>
-                <p className="text-sm">Informe o endereço onde deseja receber o pedido</p>
+          <div className="bg-stone-100 px-4 pt-9 pb-6 rounded-lg">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-2 pb-5">
+                <MapPinLine className="text-amber-600" size={20} />
+                <div>
+                  <h3 className="text-base">Endereço de entrega</h3>
+                  <p className="text-sm">Informe o endereço onde deseja receber o pedido</p>
+                </div>
               </div>
-            </div>
-            {/* <button className="hover:shadow-base p-2 rounded-full" onClick={() => setEditMode(true)}>
+              {/* <button className="hover:shadow-base p-2 rounded-full" onClick={() => setEditMode(true)}>
               <PencilSimple size={22} />
             </button> */}
-          </div>
+            </div>
 
-          <Form
-            id="checkoutForm"
-            onSubmit={handleSubmit}
-            className="grid gap-3 [grid-template:'zipCode_zipCode_._.''stt_stt_stt_.''num_num_comp_comp''neighborhood_neighborhood_city_state'45px'edit_edit_edit_edit'/_15%_15%_37%_1fr]">
-            <Input
-              required
-              disabled={isSubmitting}
-              name="zipCode"
-              className="[grid-area:zipCode]"
-              placeholder="CEP*"
-              messageError={actionData?.message?.zipCode}
-            />
-            <Input
-              required
-              disabled={isSubmitting}
-              name="street"
-              className="[grid-area:stt]"
-              placeholder="Rua*"
-              messageError={actionData?.message?.street}
-            />
-            <Input
-              required
-              disabled={isSubmitting}
-              name="number"
-              className="[grid-area:num]"
-              placeholder="Número*"
-              messageError={actionData?.message?.number}
-            />
-            <Input
-              disabled={isSubmitting}
-              name="complement"
-              className="[grid-area:comp]"
-              placeholder="Complemento"
-              messageError={actionData?.message?.complement}
-              isOptional />
-            <Input
-              required
-              disabled={isSubmitting}
-              name="neighborhood"
-              className="[grid-area:neighborhood]"
-              placeholder="Bairro*"
-              messageError={actionData?.message?.neighborhood}
-            />
-            <Input
-              required
-              disabled={isSubmitting}
-              name="city"
-              className="[grid-area:city]"
-              placeholder="Cidade*"
-              messageError={actionData?.message?.city}
-            />
-            <Select
-              required
-              name="state"
-              className="grid [grid-area:state]"
-              disabled={isSubmitting}
-              options={states as unknown as string[]}
-            />
+            <Form
+              id="checkoutForm"
+              onSubmit={handleSubmit}
+              className="grid gap-3 [grid-template:'zipCode_zipCode_._.''stt_stt_stt_.''num_num_comp_comp''neighborhood_neighborhood_city_state'45px'edit_edit_edit_edit'/_15%_15%_37%_1fr]">
+              <Input
+                required
+                disabled={isSubmitting}
+                name="zipCode"
+                className="[grid-area:zipCode]"
+                placeholder="CEP*"
+                messageError={actionData?.message?.zipCode}
+              />
+              <Input
+                required
+                disabled={isSubmitting}
+                name="street"
+                className="[grid-area:stt]"
+                placeholder="Rua*"
+                messageError={actionData?.message?.street}
+              />
+              <Input
+                required
+                disabled={isSubmitting}
+                name="number"
+                className="[grid-area:num]"
+                placeholder="Número*"
+                messageError={actionData?.message?.number}
+              />
+              <Input
+                disabled={isSubmitting}
+                name="complement"
+                className="[grid-area:comp]"
+                placeholder="Complemento"
+                messageError={actionData?.message?.complement}
+                isOptional />
+              <Input
+                required
+                disabled={isSubmitting}
+                name="neighborhood"
+                className="[grid-area:neighborhood]"
+                placeholder="Bairro*"
+                messageError={actionData?.message?.neighborhood}
+              />
+              <Input
+                required
+                disabled={isSubmitting}
+                name="city"
+                className="[grid-area:city]"
+                placeholder="Cidade*"
+                messageError={actionData?.message?.city}
+              />
+              <Select
+                required
+                name="state"
+                className="grid [grid-area:state]"
+                disabled={isSubmitting}
+                options={states as unknown as string[]}
+              />
 
-            {/* {editMode && (
+              {/* {editMode && (
               <div className="[grid-area:edit] flex justify-end relative gap-4 mt-5 animate-[slideIn_0.6s]">
                 <Button
                   disabled={isSubmitting}
@@ -167,109 +171,110 @@ export default function Checkout() {
                   {isSubmitting ? (<ArrowsClockwise className="animate-spin" size={21} />) : "salvar" }
                 </Button>
               </div>)} */}
-          </Form>
-        </div>
+            </Form>
+          </div>
 
-        <div className="bg-stone-100 px-4 py-9 rounded-lg">
-          <div className="flex gap-2">
-            <CurrencyDollar className="text-violet-600" size={25} />
-            <div>
-              <h3 className="text-base">Pagamento</h3>
-              <p className="text-sm">O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+          <div className="bg-stone-100 px-4 py-9 rounded-lg">
+            <div className="flex gap-2">
+              <CurrencyDollar className="text-violet-600" size={25} />
+              <div>
+                <h3 className="text-base">Pagamento</h3>
+                <p className="text-sm">O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+              </div>
             </div>
+
+            <div className="grid grid-cols-3 gap-3 pt-8 transition *:col-span-1 *:font-normal *:text-xs *:p-3 *:items-end">
+              <Radio
+                value="card"
+                form="checkoutForm"
+                name="paymentMethod"
+                isSelected={selectedPaymentMethod === paymentMethod.CARD}
+                onClick={() => setSelectedPaymentMethod(paymentMethod.CARD)}
+              >
+                <CreditCard className="text-violet-600" size={18} />
+                <span>Cartão</span>
+              </Radio>
+
+              <Radio
+                value="pix"
+                form="checkoutForm"
+                name="paymentMethod"
+                isSelected={selectedPaymentMethod === paymentMethod.PIX}
+                onClick={() => setSelectedPaymentMethod(paymentMethod.PIX)}
+              >
+                <PixLogo className="text-violet-600" size={18} />
+                <span>Pix</span>
+              </Radio>
+
+              <Radio
+                value="cash"
+                form="checkoutForm"
+                name="paymentMethod"
+                isSelected={selectedPaymentMethod === paymentMethod.CASH}
+                onClick={() => setSelectedPaymentMethod(paymentMethod.CASH)}
+              >
+                <Money className="text-violet-600" size={18} />
+                <span>Dinheiro</span>
+              </Radio>
+            </div>
+            {actionData?.message?.paymentMethod && (
+              <span className="text-sm text-red-400 p-1" role="alert">
+                {actionData?.message?.paymentMethod}
+              </span>
+            )}
           </div>
 
-          <div className="grid grid-cols-3 gap-3 pt-8 transition *:col-span-1 *:font-normal *:text-xs *:p-3 *:items-end">
-            <Radio
-              value="card"
-              form="checkoutForm"
-              name="paymentMethod"
-              isSelected={selectedPaymentMethod === paymentMethod.CARD}
-              onClick={() => setSelectedPaymentMethod(paymentMethod.CARD)}
-            >
-              <CreditCard className="text-violet-600" size={18} />
-              <span>Cartão</span>
-            </Radio>
-
-            <Radio
-              value="pix"
-              form="checkoutForm"
-              name="paymentMethod"
-              isSelected={selectedPaymentMethod === paymentMethod.PIX}
-              onClick={() => setSelectedPaymentMethod(paymentMethod.PIX)}
-            >
-              <PixLogo className="text-violet-600" size={18} />
-              <span>Pix</span>
-            </Radio>
-
-            <Radio
-              value="cash"
-              form="checkoutForm"
-              name="paymentMethod"
-              isSelected={selectedPaymentMethod === paymentMethod.CASH}
-              onClick={() => setSelectedPaymentMethod(paymentMethod.CASH)}
-            >
-              <Money className="text-violet-600" size={18} />
-              <span>Dinheiro</span>
-            </Radio>
-          </div>
-          {actionData?.message?.paymentMethod && (
-            <span className="text-sm text-red-400 p-1" role="alert">
-              {actionData?.message?.paymentMethod}
-            </span>
-          )}
+          <Button
+            form="checkoutForm"
+            type="submit"
+            disabled={!hasAnyItem() || isSubmitting}
+            variant="secondary"
+            className="self-end py-3 w-1/2 md:hidden"
+          >
+            {isSubmitting ? (<ArrowsClockwise className="animate-spin" size={21} />) : "finalizar pedido" }
+          </Button>
         </div>
 
-        <Button
-          form="checkoutForm"
-          type="submit"
-          disabled={!hasAnyItem() || isSubmitting}
-          variant="secondary"
-          className="self-end py-3 w-1/2 md:hidden"
-        >
-          {isSubmitting ? (<ArrowsClockwise className="animate-spin" size={21} />) : "finalizar pedido" }
-        </Button>
-      </div>
+        <div className="hidden md:block">
+          <div className="flex flex-col gap-4 py-12 h-full">
+            <h2 className="font-black text-xl font-title">Seu pedido</h2>
 
-      <div className="hidden md:block">
-        <div className="flex flex-col gap-4 py-12 h-full">
-          <h2 className="font-black text-xl font-title">Seu pedido</h2>
-
-          <div className="flex flex-col justify-between gap-8 bg-stone-100 rounded-tl-md rounded-br-md rounded-tr-[50px] rounded-bl-[50px] p-8 h-full max-w-[450px]">
-            <div className="flex flex-col items-center font-body overflow-auto max-h-72">
-              {!hasAnyItem() &&
+            <div className="flex flex-col justify-between gap-8 bg-stone-100 rounded-tl-md rounded-br-md rounded-tr-[50px] rounded-bl-[50px] p-8 h-full max-w-[450px]">
+              <div className="flex flex-col items-center font-body overflow-auto max-h-72">
+                {!hasAnyItem() &&
               (<div className="pt-10">
                 <ListMagnifyingGlass size={152} color="#d4cece" weight="bold" />
                 <span className="text-zinc-500 font-semibold">Seu carrinho está vazio</span>
               </div>)}
-              {cart.map((item) => {
-                return (
-                  <Product key={item.product.id} {...item} />
-                )
-              })}
-            </div>
+                {cart.map((item) => {
+                  return (
+                    <Product key={item.product.id} {...item} />
+                  )
+                })}
+              </div>
 
-            <div>
-              <Total
-                productsPrice={productsPrice}
-                delivery={delivery}
-                total={productsPrice + delivery}
-              />
+              <div>
+                <Total
+                  productsPrice={productsPrice}
+                  delivery={delivery}
+                  total={productsPrice + delivery}
+                />
 
-              <Button
-                form="checkoutForm"
-                type="submit"
-                disabled={!hasAnyItem() || isSubmitting}
-                variant="secondary"
-                className="py-3 w-full"
-              >
-                {isSubmitting ? (<ArrowsClockwise className="animate-spin" size={21} />) : "finalizar pedido" }
-              </Button>
+                <Button
+                  form="checkoutForm"
+                  type="submit"
+                  disabled={!hasAnyItem() || isSubmitting}
+                  variant="secondary"
+                  className="py-3 w-full"
+                >
+                  {isSubmitting ? (<ArrowsClockwise className="animate-spin" size={21} />) : "finalizar pedido" }
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
